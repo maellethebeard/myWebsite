ff024ce2efd0f7af21329fb3bbfdafc734979d94
Maelle Girard <maelle.labarbe@gmail.com>
2016-02-16 22:11:32 +0800

    Adding images + improvement of skills box

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
 	console.log("Ready ?");
 	$( window ).scroll(function() {
-		var myScroll = 	$(window).scrollTop();
-		myScroll = 1 - myScroll/150;
-		$("h1").css("opacity", myScroll);
-		console.log(myScroll);
+		var windowsTop = 	$(window).scrollTop();
+		var myScrollTitle = 1 - windowsTop/150;
+		$("h1").css("opacity", myScrollTitle);
+		console.log(windowsTop);
+
+		$('.skills-box').each( function(i){
+			var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            	if( bottom_of_window > bottom_of_object ){
+                
+               	 $(this).delay(i*170).animate({'opacity':'1'},800);
+                    
+           		 };
+        });
 
 	});
+
 });
